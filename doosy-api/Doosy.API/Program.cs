e3a743122d1a1ac84e634b69ffe88a340817bee4
kekgaugetswes <kekgaugetswe@doosy.co.za>
2022-01-21 11:04:10 +0200

    update

--- doosy-api/Doosy.API/Program.cs
+++ doosy-api/Doosy.API/Program.cs
@@ -15,10 +15,12 @@ builder.Services.AddDomain();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
 
 app.UseCors(builders => builders.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
